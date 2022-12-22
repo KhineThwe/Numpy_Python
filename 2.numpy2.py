6c30a169d213fbafc503f47ce5983273dc3d9027
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-12-21 17:42:48 -1200

    Create 2.numpy2.py

---
+++ 2.numpy2.py
@@ -0,0 +1,17 @@
+#JupyterNoteBook ----> Web page interactive environment
+#desktop application or software တွေမှာ လုပ်လို့ရမဲ့လုပ်ဆောင်ချက်တွေကို web page or browser ပေါ်မှာသွားရောက်လုပ်တာ
+#jupyter notebookကို computer analysis နဲ့ problem solving,machine learning တွေမှာများသောအားဖြင့် အသုံးပြုသည်။
+#image processing ,video processing တွေမှာဆိုလည်းအတော်အသုံးဝင်
+#math နဲ့ပတ်သတ်တဲ့ equation တွေကိုလည်းသုံး
+#line by line result stage ကိုလည်း check နိုင်တယ်
+#html js တို့နှင့်တွဲပြီး user interface တွေလည်း ဖန်တီးလို့ရသေးတယ်
+import numpy
+arr = numpy.array
+print(arr)
+#class type ndimensional array
+# import numpy as np
+# arr = np.array([
+#     [1,2,3],[4,5,6],[7,8,9]
+# ])
+# print(arr[0])
+# print(type(arr))
