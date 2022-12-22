a2e29fbbd0d1a0fbea83bbbae2013855679119ab
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-12-21 12:39:30 -1200

    Create 1.numpy1.py

---
+++ 1.numpy1.py
@@ -0,0 +1,23 @@
+#numpy
+#1-numpy.py
+#numpy ---> numerical python
+#python library for numerical computing and scienticfic computing
+#multidimensional array,matrices,mathematical functions,logical,shape manipulation,sorting, selecting,
+#discrete fourier transform,basic linear algebra,basic statistical operations,random simulation
+#calculation nae sine tae hr akone pr dal
+#complex ဖြစ်လွန်းတဲ့ကိန်းဂဏန်းတွေကို ကိုင်တွယ်ချင်တဲ့အခါ သုံးတယ်
+#numpy ၏ c programming language နဲ့ရေးထားတာဖြစ်တယ်
+#python မှာ built-in ပါတဲ့ ကောင်တွေက code တွေအများကြီးပိုရေးရပြီးတော့ po efficiency nal tal
+#numpy array vs python array
+#အသေသတ်မှတ်ပြီးသား numpy array ka ---> staticပြောင်းချင်သလိုပြောင်းလို့မရ
+#python array တွေက dynamic allocation ပြောင်းချင်သလိုပြောင်းလိုံရ
+#numpy first array --> Ndarray(N dimensional array)
+#Ndimensional array mhar so တူညီတာတွေသာ သိမ်းပြီး‌တော့ 0 ခန်းကနေစတယ်
+#python mhar so ---> list
+#array တစ်ခုထဲမှာမတူညီတဲ့ data type တွေထည့်ထားတယ်ဆိုလို့ရှိရင် int == 4 bytes pl htr string = 8bytes ဆိုတော့ data length တွေသည် တူညီမှုမရှိတော့ဘူး
+#numpy array တွေသည်data type တွေတူညီလို့သူ့ကို homogeneous data လို့ခေါ်တယ်
+#python array ---> heterogeneous data
+#to install numpy library ----> pip3 install --user numpy
+import numpy as np
+arr = np.array#numpy array 1 ku create
+print(arr)
