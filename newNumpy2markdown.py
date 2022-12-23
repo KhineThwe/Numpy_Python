15a2bc56805f79d8a70ee5f8b754e4d94d26bf61
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-12-23 00:12:52 -1200

    Create newNumpy2markdown.py

---
+++ newNumpy2markdown.py
@@ -0,0 +1,31 @@
+#jupter note book က JSON ပုံစံstore တယ်
+#extension ipynb -->  i python notebook
+#programming တစိခုနဲ့ တစ်ခုကို Kernel ထပ် down ပြီးချိန်းလို့ရတယ်
+#code က program အတွက် 
+#markdown က ** ဆို italic ပုံစံပြ
+# ----> # Hello ဆို heading ပုံစံ ပိုကြီး -----> ## Hello ဆို font size ပိုသေး
+#math equation တွေကိုဖော်ပြလိုလျှင် လည်း markdown သုံးနိုင်တယ် 
+#တစ်ကွက်ချင်းစီကို cell လို့ခေါ်တယ်
+#run ရင် shift enter and ctrl enter ရတယ်
+#Shift+enter ဆို new cell ပေါ်ပေး ဖြတ်ဖြတ်run ချင်ရင် 
+#ctrl+enter က cell အသစ်ထပ်မပေါ်ပေးတော့ဘူး
+
+
+#Markdown သည် HTML ၏ sub တစ်ခုဖြစ်လို့ ယူသုံးလို့ရတယ်
+#same location မဟုတ်ရင်နေရာအတိအကျထည့်လို့ရတယ်
+
+
+<img src="image.png"> 
+#in markdown mode
+#image processing
+
+#for video in markdown mode
+<video controls src="video_sample.avi">myVideo</video>
+
+
+#in cmd prompt
+#pip3 install --user convert
+#jupyter nbconvert --to html filename
+
+#if err
+#python -m pip install jupyter --user
