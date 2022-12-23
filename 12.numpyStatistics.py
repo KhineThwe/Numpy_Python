77ed5dc6d6ce23faec77bfbdfc835fd24463100e
Khine Zar Thwe <57827636+KhineThwe@users.noreply.github.com>
2022-12-22 18:40:30 -1200

    Create 12.numpyStatistics.py

---
+++ 12.numpyStatistics.py
@@ -0,0 +1,19 @@
+#Statistics
+#max min etc
+
+import nump as np
+stats = np.array([[1,2,3],[4,5,6]])
+stats
+
+
+np.max(stats)
+np.min(stats)
+
+np.min(stats,axis=1)
+
+
+np.max(stats,axis=1)
+np.sum(stats)
+
+np.sum(stats,axis=0)
+#adding row by row
